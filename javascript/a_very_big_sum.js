a761dd6a7eb3fe320e7188800246af01f822f808
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-06-01 01:13:18 -0700

    Added a very big sum problem

---
+++ javascript/a_very_big_sum.js
@@ -0,0 +1,14 @@
+function aVeryBigSum() {
+  let result = 0
+  const length = ar.length
+
+  for (let i = 0; i < length; i++) {
+    result = ar[i] + result
+  }
+  
+  return result
+}
+
+// run
+const ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+aVeryBigSum(ar)

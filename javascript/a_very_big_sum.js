ed22b24798dccfd9d0d4b3bb55664378a2eb8764
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-06-01 01:30:26 -0700

    Link reference added

--- javascript/a_very_big_sum.js
+++ javascript/a_very_big_sum.js
@@ -12,3 +12,6 @@ function aVeryBigSum() {
 // run
 const ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
 aVeryBigSum(ar)
+
+// reference
+// https://www.hackerrank.com/challenges/a-very-big-sum/problem

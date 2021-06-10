c009a5d9ef397ffc7c3bc4499facc17ecb80d9f3
Israel Martinez <israel.martinez.vargas@gmail.com>
2021-06-09 18:11:04 -0700

    Create migratory_birds.js
    migratory-birds solution added

---
+++ javascript/migratory_birds.js
@@ -0,0 +1,25 @@
+  function migratoryBirds(arr) {
+    let max = 0
+    let birds = {}
+    let common = null
+
+    for (let a = 0; a <= arr.length; a++) {
+        const bird = arr[a]
+        birds[bird] = birds[bird] ? birds[bird] + 1 : 1;
+    }
+    
+    for (const b in birds) {
+        if (birds[b] > max) {
+            common = b;
+            max = birds[b];
+        }
+    }
+    
+    return common;
+}
+
+const arr1 = [1, 4, 4, 4, 5, 3];
+
+console.log(migratoryBirds(arr1))
+
+// expected 4

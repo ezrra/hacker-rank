a991ed95596c8b3db7efd3c4ca334da98961b767
Israel Martinez <israel.martinez.vargas@gmail.com>
2021-06-21 10:12:04 -0700

    Refactor

--- javascript/left_rotation.js
+++ javascript/left_rotation.js
@@ -1,9 +1,7 @@
 // https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
 
 function rotLeft(a, d) {
-  const part = a.slice(0, d)
-  a.splice(0, d)
-  return [...a, ...part]
+  return [...a.slice(d), ...a.slice(0, d)]
 }
 
 console.log(rotLeft([1, 2, 3, 4, 5], 4))
